917ff0fb3ddb5511fb920bfac1e381cb1f9d04da
fangzhensheng <1613220541@qq.com>
2024-08-28 20:36:55 +0800

    Update json_utils.py

--- json_utils.py
+++ json_utils.py
@@ -1,16 +1,15 @@
-# json_utils.py
 import json
 import jsonlines
 
 def load_json(file_path):
     print("file path here: ", file_path)
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
 def load_jsonl(file_path):
     print("file path here: ", file_path)
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         raw_data = file.readlines()
     data = []
     for line in raw_data:
